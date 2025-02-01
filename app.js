3a4740f989d67b39a50f8fb1a3c65eb7d2699bff
Jermy1234 <dvirdavid201201@gmail.com>
2025-02-01 20:33:10 +0200

    calculator

---
+++ app.js
@@ -0,0 +1,28 @@
+const screenDisplay = document.querySelector('.screen')
+const buttons = document.querySelectorAll('button')
+console.log(buttons)
+let calculation = []
+
+// calc will push the clicked buttons into the array and 
+// will make the console showcase it. 
+function calculate(button) {
+    console.log('clicked', button.textContent)
+    const value = button.textContent
+
+    if (value === "CLEAR") {
+        calculation = [] 
+        screenDisplay.textContent = '.'
+
+    } else if (value == "=") {
+        let result = new Function('return' + accumulativeCalculation.replace(/x/g, '*').replace(/÷/g, '/'))
+        ();
+        screenDisplay.textContent = result;
+            //screenDisplay.textContent = eval(accumulativeCalculation)
+    } else {
+        calculation.push(value)
+        accumulativeCalculation = calculation.join('')
+        screenDisplay.textContent = accumulativeCalculation
+    }
+}
+buttons.forEach
+    (button=> button.addEventListener('click', ()=> calculate(button)))
